7f1538a66246409fe3dfb6bdd10718dc7c4da91d
JagadishBujji <jagadish00198@gmail.com>
2024-01-05 17:35:10 +0530

    ui changes

--- src/components/MobileOurContact.jsx
+++ src/components/MobileOurContact.jsx
@@ -19,58 +19,56 @@ const MobileOurContact = () => {
     <>
       <div className={`${classes.Contactsmobile} zoom-animation`}>
         <div className={`${classes.Contactcontainer} container`}>
-          <div className={`${classes.Contactcontainer} container`}>
-            <div className={`${classes.Servicescontent1} `}>
-              <h3 className={classes.contacttitle}>Contact</h3>
-              <p className={classes.para1}>
-                {" "}
-                <img
-                  src="/images/Mask group.svg"
-                  alt=""
-                  className={classes.clientsimg}
-                />
-                +91 98401 51021
-              </p>
-              <p className={classes.para1}>
-                {" "}
-                <img
-                  src="/images/Mask group (1).svg"
-                  alt=""
-                  className={classes.clientsimg}
-                />
-                hey@twentyonest.in
-              </p>
-              <div className={classes.address1}>
-                <img
-                  src="/images/Chennai.png"
-                  alt=""
-                  className={classes.addressimg}
-                />
-                <div className={classes.addresscontent}>
-                  <h3 className={classes.addresstitle}>CHENNAI</h3>
-                  <p className={classes.add1}>
-                    No. 40, Rukmani Street,
-                    <br /> West Mambalam, Chennai,
-                    <br /> Tamil Nadu 600 033
-                  </p>
-                </div>
+          <div className={`${classes.Servicescontent1} `}>
+            <h3 className={classes.contacttitle}>Contact</h3>
+            <p className={classes.para1}>
+              {" "}
+              <img
+                src="/images/Mask group.svg"
+                alt=""
+                className={classes.clientsimg}
+              />
+              +91 98401 51021
+            </p>
+            <p className={classes.para1}>
+              {" "}
+              <img
+                src="/images/Mask group (1).svg"
+                alt=""
+                className={classes.clientsimg}
+              />
+              hey@twentyonest.in
+            </p>
+            <div className={classes.address1}>
+              <img
+                src="/images/Chennai.png"
+                alt=""
+                className={classes.addressimg}
+              />
+              <div className={classes.addresscontent}>
+                <h3 className={classes.addresstitle}>CHENNAI</h3>
+                <p className={classes.add1}>
+                  No. 40, Rukmani Street,
+                  <br /> West Mambalam, Chennai,
+                  <br /> Tamil Nadu 600 033
+                </p>
               </div>
-              <div className={classes.address1}>
-                <img
-                  src="/images/Bengaluru.png"
-                  alt=""
-                  className={classes.addressimg}
-                />
-                <div className={classes.addresscontent}>
-                  <h3 className={classes.addresstitle}>BENGALURU</h3>
-                  <p className={classes.add1}>
-                    Novel Business Park, #57,
-                    <br /> 13th Cross, Baldwins Road,
-                    <br />
-                    Koramangala, Bengaluru,
-                    <br /> Karnataka 560 030
-                  </p>
-                </div>
+            </div>
+            <div className={classes.address1}>
+              <img
+                src="/images/Bengaluru.png"
+                alt=""
+                className={classes.addressimg}
+              />
+              <div className={classes.addresscontent}>
+                <h3 className={classes.addresstitle}>BENGALURU</h3>
+                <p className={classes.add1}>
+                  Novel Business Park, #57,
+                  <br /> 13th Cross, Baldwins Road,
+                  <br />
+                  Koramangala, Bengaluru,
+                  <br /> Karnataka 560 030
+                </p>
               </div>
             </div>
           </div>
